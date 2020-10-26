f972a03cf9c9080946b35037b2dff20231458758
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2020-10-26 11:22:54 +0900

    cairo code

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use sdl2;
+use cairo::{Context, Format, ImageSurface};
+use sdl2::event::Event;
+use sdl2::pixels::{Color, PixelFormatEnum};
+use sdl2::rect::Rect;
+use sdl2::render::BlendMode;
+
+fn main() -> anyhow::Result<()> {
+    let sdl_context = sdl2::init().map_err(anyhow::Error::msg)?;
+    let video_subsystem = sdl_context.video().map_err(anyhow::Error::msg)?;
+    let window = video_subsystem
+        .window("", 240, 240)
+        // .allow_highdpi()
+        .build()?;
+    let mut canvas = window.into_canvas().build()?;
+
+    let (width, height) = (120, 120);
+    let texture_creator = canvas.texture_creator();
+    let mut texture = texture_creator.create_texture_streaming(
+        PixelFormatEnum::ARGB8888,
+        width as u32,
+        height as u32,
+    )?;
+    texture.set_blend_mode(BlendMode::Blend);
+    texture
+        .with_lock(None, |pixels, stride| -> Result<_, anyhow::Error> {
+            let pixels =
+                unsafe { std::slice::from_raw_parts_mut(pixels.as_mut_ptr(), pixels.len()) };
+            let surface = ImageSurface::create_for_data(
+                pixels,
+                Format::ARgb32,
+                width,
+                height,
+                stride as i32,
+            )?;
+            // let surface = unsafe {
+            //     ImageSurface::create_for_data_unsafe(pixels.as_mut_ptr(), Format::ARgb32, width, height, stride as i32)
+            // }?;
+
+            let cr = Context::new(&surface);
+            cr.set_source_rgb(1.0, 0.0, 0.0);
+            cr.rectangle(30.0, 30.0, 90.0, 90.0);
+            cr.fill();
+            Ok(())
+        })
+        .map_err(anyhow::Error::msg)??;
+
+    canvas.set_draw_color(Color::RGBA(20, 20, 20, 0));
+    canvas.clear();
+    canvas
+        .copy(&texture, None, Rect::new(0, 0, 120, 120))
+        .map_err(anyhow::Error::msg)?;
+    canvas.present();
+
+    for event in sdl_context
+        .event_pump()
+        .map_err(anyhow::Error::msg)?
+        .wait_iter()
+    {
+        if let Event::Quit { .. } = event {
+            break;
+        }
+    }
+
+    Ok(())
+}
